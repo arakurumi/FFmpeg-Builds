43b1f796d5b2800366fc0d1fb2139a3c1529f3df
Kurumi <kqruumi@gmail.com>
2026-09-14 10:43:13 +0700

    feat: custom variants

--- util/release_body.sh
+++ util/release_body.sh
@@ -9,7 +9,7 @@ fi
 TAGNAME="$1"
 shift
 
-REPO="${GITHUB_REPOSITORY:-BtbN/FFmpeg-Builds}"
+REPO="${GITHUB_REPOSITORY:-arakurumi/FFmpeg-Builds}"
 DL_BASE="https://github.com/${REPO}/releases/download/${TAGNAME}"
 
 TARGETS=(win64 winarm64 win32 linux64 linuxarm64 linux32)
